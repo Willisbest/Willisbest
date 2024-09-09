1eb5e0c99f4f480ecbd108dc979f5411537c9fe3
Willisbest <karlmoessinger@gmail.com>
2024-09-09 18:06:50 +0200

    update from work again

--- python_kurs_ab_129.py
+++ python_kurs_ab_129.py
@@ -1,19 +1,108 @@
-from functools import reduce
+#lambda practicetasks
+# #Sqare it using lambda
+# my_list = [5,4,3]
 
-my_list = [1,2,3]
+# new_list = list(map(lambda item: item*item, my_list))
 
-def multiply_by2(item):
-    return item * 2
+# print(new_list)
 
-def only_odd(item):
-    return item % 2 != 0 #If the remeinder of x/2 does not equal 0 its not odd
+#sort list where second value is smallest first and largest last using lambda
 
-def accumulator(acc, item):
-    print(acc, item)
-    return acc + item
+a= [(0,2),(4,3),(10,-1),(9,9)]
+a.sort(False)
+print(a)
 
-print(reduce(accumulator, my_list, 0))
-print(my_list)
+
+
+
+
+
+
+
+
+# lambda expressions
+# lambda param: action(param)
+
+
+# from functools import reduce
+
+# my_list = [1,2,3]
+
+# def accumulator(acc, item):
+#     print(acc, item)
+#     return acc + item
+
+# print(reduce(accumulator, my_list, 0))
+# print(my_list)
+
+
+
+
+
+
+
+
+
+#Exercise for the functions
+# from functools import reduce
+
+# # #1 Capitalize all of the pet names and print the list
+# # my_pets = ['sisi', 'bibi', 'titi', 'carla']
+
+# # # def capitalize_pet(pet):
+# # #     return pet.capitalize()
+
+# # print(list(map(str.capitalize, my_pets)))
+
+
+# #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+# # my_strings = ['a', 'b', 'c', 'd', 'e']
+# my_numbers = [5,4,3,2,1]
+
+# # # my_numbers.sort()
+
+# # print(list(zip(sorted(my_numbers), my_strings)))
+
+# #3 Filter the scores that pass over 50%
+# scores = [73, 20, 65, 19, 76, 100, 88]
+
+# # def over_50(num):
+# #     if (num>50):
+# #         return num
+        
+# #print(sorted(list(filter(over_50, scores))))
+
+
+# #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+
+# def accumulator(acc, item):
+#     return acc + item
+
+# print(reduce(accumulator, scores + my_numbers, 0))
+# print(sum(my_numbers+scores))
+
+
+
+
+
+
+
+# from functools import reduce
+
+# my_list = [1,2,3]
+
+# def multiply_by2(item):
+#     return item * 2
+
+# def only_odd(item):
+#     return item % 2 != 0 #If the remeinder of x/2 does not equal 0 its not odd
+
+# def accumulator(acc, item):
+#     print(acc, item)
+#     return acc + item
+
+# print(reduce(accumulator, my_list, 0))
+# print(my_list)
 
 
 
