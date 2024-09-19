f3e99fc97f65b6bd86af57b2b62e95c5edda06bf
Willisbest <karlmoessinger@gmail.com>
2024-09-19 02:16:05 +0200

    commiting the stuff from pycharm with a dedicated newly created package

--- Packages_Modules_Learning/main.py
+++ Packages_Modules_Learning/main.py
@@ -1,4 +1,8 @@
-import utility
-import shopping.shopping_cart
+from utility import *
+from shoppingg.new import shopping_cart
 
-print(shopping.shopping_cart.buy("apple"))
+print(shopping_cart.buy("apple"))
+print(divide(5,2))
+print(multiply(5,2))
+print(max[1,2,3])
+<
